9b689e987dda9f09dc65430d1a5611819fb07956
nailuj29gaming <54189157+nailuj29gaming@users.noreply.github.com>
2019-09-14 12:59:46 -0400

    Update buttonPress.ino
    prevented servo from burning out

--- buttonPress.ino
+++ buttonPress.ino
@@ -5,13 +5,14 @@ Servo myServo;
 void setup() {
   // put your setup code here, to run once:
   myServo.attach(SERVOPIN);
+  myServo.write(90);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   myServo.write(0);
   delay(500);
-  myServo.write(45);
+  myServo.write(15);
   delay(500);
   
  }
